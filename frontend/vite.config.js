77495bfc219727c4ae763cf3c06e293727e37c6e
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-25 20:04:48 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': ['http://localhost:5000/', 'https://7335-2400-adc5-16a-a200-1060-7de4-8e99-9cf.ngrok-free.app/'],
+      '/api': ['https://7335-2400-adc5-16a-a200-1060-7de4-8e99-9cf.ngrok-free.app/'],
     },
   },
   plugins: [react()],
