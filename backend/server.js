ae9f1aed3931c12cecc6d89fb29d439f34e94af0
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-25 16:41:03 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const speechRoutes = require('./routes/speech');
-const audioRoutes = require('./routes/audio');
-const yoloRoutes = require('./routes/yolo');
-const chatRoutes= require('./routes/chat');
+// const speechRoutes = require('./routes/speech');
+// const audioRoutes = require('./routes/audio');
+// const yoloRoutes = require('./routes/yolo');
+// const chatRoutes= require('./routes/chat');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+//     credentials: true
+// }));
 
 // Routes
-app.use('/api/audio', audioRoutes);
-app.use('/api/yolo', yoloRoutes);
-app.use('/api', speechRoutes);
-app.use('/api', chatRoutes);
+// app.use('/api/audio', audioRoutes);
+// app.use('/api/yolo', yoloRoutes);
+// app.use('/api', speechRoutes);
+// app.use('/api', chatRoutes);
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+// });
 
 app.get('/', (req, res) => {
     res.send('Server is running');
